50807e0610e27eaade0dd91197e8503182a466af
JJWRoeloffs <jelleroeloffs@gmail.com>
2024-05-22 19:57:56 +0200

    Update runscript to version used for results

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,16 @@
+#!/usr/bin/env bash
+
+python -m zip_lm.scripts.evaluate_random 42 100
+
+python -m zip_lm.scripts.evaluate_ngrams 42 100 1 -d 10M
 python -m zip_lm.scripts.evaluate_ngrams 42 100 2 -d 10M
+
+python -m zip_lm.scripts.evaluate_bootstrapzipmodel 42 100 1000 1000 -t threequarterq -d 10M
 python -m zip_lm.scripts.evaluate_bootstrapzipmodel 42 100 1000 1000 -t halfq -d 10M
 python -m zip_lm.scripts.evaluate_bootstrapzipmodel 42 100 1000 1000 -t quarterq -d 10M
+
+python -m zip_lm.scripts.evaluate_softmaxzipmodel 42 100 10 -t threequarterq -d 10M
 python -m zip_lm.scripts.evaluate_softmaxzipmodel 42 100 10 -t halfq -d 10M
+python -m zip_lm.scripts.evaluate_softmaxzipmodel 42 100 10 -t quarterq -d 10M
+
+python -m zip_lm.scripts.evaluate_t5 42 100 --model_name t5-3b
